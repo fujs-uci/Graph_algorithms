a4d975031b37b692ec017a35f20a588654abb69d
Justin F <fu.justin.s@gmail.com>
2018-05-02 13:08:42 -0700

    Update Reachability.py

--- Reachability.py
+++ Reachability.py
@@ -4,9 +4,12 @@ class reachabilty:
         #Reachability will focus on DFS and BFS
         # might add more algorithms later
 
+        #could implement strongly connected with this as well
+
         def __init__(self, graph):
                 self._graph = graph
                 self._DFS = None
+                self._BFS = None
 
         def getGraph(self):
                 return self._graph
@@ -32,12 +35,34 @@ class reachabilty:
 
         def displayDFS(self, start):
                 #calls DFS and displays the path it took.
-                self.DFS(0)
+                self.DFS(start)
                 print(self._DFS)
                                               
         
                 
         def BFS(self, start):
+                #BDF search algorithm
+                reached = [self.getGraph().getVertex(start)]
+                queue = [self.getGraph().getVertex(start)]
+                while(len(queue) != 0):
+                        curr_vertex = queue[0]
+                        queue = queue[1:]
+                        for vertex in curr_vertex.getOutgoing():
+                                visit_vertex = self.getGraph().getVertex(vertex) 
+                                if visit_vertex not in reached:
+                                        reached.append(visit_vertex)
+                                        queue.append(visit_vertex)
+                        
+
+
+                self._BFS = [v.display() for v in reached]
+
+        def displayBFS(self, start):
+                self.BFS(start)
+                print(self._BFS)
+                        
+        def findSCC(self):
+                #returns all the scc
                 pass
 
 
@@ -47,3 +72,7 @@ Graph.printAdjList()
 
 Reach = reachabilty(Graph)
 Reach.displayDFS(0)
+Reach.displayBFS(0)
+
+x = [1,2,3]
+print(x[1:])
